82325df9d35d405addbf252739e78121b7a7ec8b
DrBuschle <dr.buschle@hotmail.com>
2020-07-07 11:35:13 +0200

    Newest_updates

--- R_Practical_Machine_Learning_Coursera/2nd_Week/Week_2_lecture_6_Basic_Covariate_creation.R
+++ R_Practical_Machine_Learning_Coursera/2nd_Week/Week_2_lecture_6_Basic_Covariate_creation.R
@@ -3,207 +3,108 @@
 
 rm(list = ls())
 
-# Week 2 lecture 5 - Basic preprocessing  ####
+# Week 2 lecture 6 - Covariate creation  ####
 
 
-# Why preprocess?
+# Two levels of covariate creation ####
 
 
-library(caret)
+# Level 2: Transforming tidy covaraibles
+
 library(kernlab)
 data(spam)
 
-set.seed(1235)
+# square it
+spam$capitalAveSq <- spam$capitalAve^2
 
-inTrain <- createDataPartition(y = spam$type, p = 0.75, list = F)
 
-training <- spam[inTrain, ]
-testing <- spam[-inTrain, ]
 
-# example for a very skewed variable
-# most of the values are small, but there are outliers with huge values
 
-hist(training$capitalAve, main = "", xlab = "ave. capital run length")
 
-mean(training$capitalAve)
-# [1] 5.074
 
-sd(training$capitalAve)
-# [1] 29.61
+# Load example data ####
 
+library(ISLR)
+library(caret)
+data(Wage)
 
+inTrain <- createDataPartition(y = Wage$wage, p = 0.7, list = FALSE)
+training <- Wage[inTrain, ]
+testing <- Wage[-inTrain, ]
 
 
 
+# Common covariates to add, dummy variables ####
 
+# Basic idea - convert factor variable to indicator variables
 
-# Standardizing - training set
+table(training$jobclass)
+# 1. Industrial 2. Information 
+# 1062           1040 
 
-trainCapAve <- training$capitalAve
 
-# typical standarization: take the value, subtract the mean and this result gets divided by its standard deviation
-trainCapAveS <- (trainCapAve - mean(trainCapAve)) / sd(trainCapAve)
+# -> this is hard to use for prediction algorithms to use this information as it is 
+# -> these are qualitative variable and have to be turned into quantitative variables
 
-mean(trainCapAveS)
-# [1] -1.206e-17    <- mean gets very close to 0
 
-sd(trainCapAveS)
-# [1] 1   <- standard deviation gets 1
+jobclass_dummies <- dummyVars(wage ~ jobclass, data = training)
+# wage is the outcome
+# jobclass is the predictor variable
+# traing set is where the dummy variable are build
 
-# -> strong reduction of variability and standarize the variables
+head(predict(jobclass_dummies, newdata = training))
+# allpy a prediction on the "jobclass_dummies" and the training set
+# use "newdata" instead of "data"
 
 
+# jobclass.1. Industrial jobclass.2. Information
+# 231655                      1                       0
+# 86582                       0                       1
+# 161300                      1                       0
+# 155159                      0                       1
+# 11443                       0                       1
+# 376662                      0                       1
 
 
 
-# Standardizing - test set
-# WHEN YOU APPLY a standardization to a training set, you also have to do that for the test set!!!!
-# BUT YOU HAVE TO use the values from the TRAINING SET for standadizing the TEST SET!!!!
 
+# Removing zero covariates ####
 
-testCapAve <- testing$capitalAve
+# Sometimes some of the variables have no variablity 
+# e.g does an e-mail have a letter in it: yes/ no -> not a useful covariate, since nearly all mails do have a letter
 
-# typical standarization: take the value, subtract the mean and this result gets divided by its standard deviation
-testCapAveS <- (testCapAve - mean(trainCapAve)) / sd(trainCapAve)
+# find variables with low variability, which are most likely no good predictors
 
-mean(testCapAveS)
-# [1] 0.01588    <- mean gets in the direction of 0, but most likley not too close
+nsv <- nearZeroVar(training, saveMetrics = T)
+# saveMetrix = T: so you can see how it was calculated
 
-sd(testCapAveS)
-# [1] 1.262     <- standard deviation gets close to 1, but most likley not too close
+nsv
 
 
+#              freqRatio percentUnique zeroVar   nzv
+# year        1.028736    0.33301618   FALSE FALSE
+# age         1.014286    2.85442436   FALSE FALSE
+# maritl      3.134783    0.23786870   FALSE FALSE
+# race        8.705000    0.19029496   FALSE FALSE
+# education   1.392562    0.23786870   FALSE FALSE
+# region      0.000000    0.04757374    TRUE  TRUE
+# jobclass    1.021154    0.09514748   FALSE FALSE
+# health      2.462932    0.09514748   FALSE FALSE
+# health_ins  2.289515    0.09514748   FALSE FALSE
+# logwage     1.037037   19.31493815   FALSE FALSE
+# wage        1.037037   19.31493815   FALSE FALSE
 
+# percentUnique: percentage of unqiue values in the data set
+# zeroVar: zero variable
+# nzv: near zero variable
 
-# Standardization - preProcessfunction
 
-# column 58 is the one we care about ("type"), it is the one about spam or not spam
-preObj <- preProcess(training[ , -58], method = c("center", "scale"))
-# method = c("center", "scale"): does what we did in the example before: 
-# take the value, subtract the mean and this result gets divided by its standard deviation
+# -> region is a zero variable, subsequently a bad predictor and needs to be thrown out 
 
-trainCapAveS <- predict(preObj, training[ , -58])$capitalAve
-mean(trainCapAveS)
 
-# [1] -1.206e-17    <- same value like in the example before
 
-sd(trainCapAveS)
 
-# [1] 1       <- same value like in the example before
 
-
-# the preObj can also be applied on the test set directly
-
-testCapAveS <- predict(preObj, testing[ , -58])$capitalAve
-mean(testCapAveS)
-# [1] 0.01588
-
-sd(testCapAveS)
-# [1] 1.262
-
-
-
-
-
-# Standardization - preProcessargument
-# you can directly tell the train function to use the preProcess argument and specifiy it
-
-set.seed(32343)
-modelFit <- train(type ~., data = training, preProcess = c("center", "scale"), method = "glm")
-modelFit
-
-# Generalized Linear Model 
-# 
-# 3451 samples
-# 57 predictor
-# 2 classes: 'nonspam', 'spam' 
-# 
-# Pre-processing: centered (57), scaled (57) 
-# Resampling: Bootstrapped (25 reps) 
-# Summary of sample sizes: 3451, 3451, 3451, 3451, 3451, 3451, ... 
-# Resampling results:
-#   
-#   Accuracy  Kappa 
-# 0.9203    0.8327
-
-
-
-
-# Standardizing - Box-Cox transforms
-# other kind of transformation
-
-preObj <- preProcess(training[ , -58], method = c("BoxCox"))
-# BoxCox takes continuous data and try to make them look like normal data
-# they do that by estimating a specific set of parameters using maximum likelihood
-
-trainCapAveS <- predict(preObj, training[ , -58])$capitalAve
-par(mfrow = c(1,2))
-hist(trainCapAveS)
-qqnorm(trainCapAveS)
-
-
-# at the original plot there was a huge bar at around 0
-
-
-
-
-
-
-# Standardizing - Imputing data ("guess" NA values)
-
-set.seed(13343)
-
-# many algorithms have problems with missing values
-# therefore we will introduce some, just to check how to handle this
-
-# Make some values NA
-training$capAve <- training$capitalAve
-selectNA <- rbinom(dim(training)[1], size = 1, prob = 0.05) == 1
-training$capAve[selectNA] <- NA
-
-
-# Impute (anrechnen) and standardize
-preObj <- preProcess(training[ , -58], method = "knnImpute")
-# knnImpute: k nearest neightbours Imputation
-# finds the k nearest neighbours, which look most like the missing value and puts the average value to the missing one
-
-# install.packages("RANN")
-
-capAve <- predict(preObj, training[ , -58])$capAve
-
-
-# Standardize true value
-capAveTruth <- training$capitalAve
-capAveTruth <- (capAveTruth-mean(capAveTruth)) / sd(capAveTruth)
-
-
-
-# Standardize - Imputing data
-
-# all value (including imputed values) - true values
-quantile(capAve - capAveTruth)
-
-# 0%        25%        50%        75%       100% 
-# -12.541920  -0.013119  -0.008136   0.002258   4.588768 
-
-# around 50 % the difference is close to zero, here it worked well
-# at 0 % and 100 % the differences are much larger, here it didn't work too well
-
-
-# only imputed values - true values
-quantile((capAve - capAveTruth)[selectNA])
-# 0%        25%        50%        75%       100% 
-# -12.541920  -0.017931  -0.005889   0.017628   1.178953 
-
-# -> the value should be more variable
-
-
-# only NOT imputed values - true values
-quantile((capAve - capAveTruth)[!selectNA])
-
-# 0%       25%       50%       75%      100% 
-# -0.017023 -0.012991 -0.008192  0.001791  4.588768 
-
-# the values should be closer to each other
+# Spline basis ####
 
 
