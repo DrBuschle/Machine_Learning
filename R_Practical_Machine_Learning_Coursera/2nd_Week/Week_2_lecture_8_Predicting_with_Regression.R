d83d6fd1f4caf53ad9c7e1c0466a69221616a324
DrBuschle <dr.buschle@hotmail.com>
2020-07-08 23:39:47 +0200

    Newest_updates

--- R_Practical_Machine_Learning_Coursera/2nd_Week/Week_2_lecture_8_Predicting_with_Regression.R
+++ R_Practical_Machine_Learning_Coursera/2nd_Week/Week_2_lecture_8_Predicting_with_Regression.R
@@ -124,13 +124,77 @@ lines(testFaith$waiting, predict(lm1, newdata = testFaith), lwd = 3)
 # Get training set/ test set errors ####
 
 # Calculate RMSE on training
-
 sqrt(sum((lm1$fitted.values - trainFaith$eruptions)^2))
 # [1] 5.75186
 
-# Calculate RMSE on test
 
+# Calculate RMSE on test
 sqrt(sum((predict(lm1, newdata = testFaith) - testFaith$eruptions)^2))
 # [1] 5.838559
+
+
+
+
+
+# Prediction intervals ####
+
+par(mfrow = c(1,1))
+pred1 <- predict(lm1, newdata = testFaith, interval = "prediction")
+ord <- order(testFaith$waiting)
+plot(testFaith$waiting, testFaith$eruptions, pch = 19, col = "blue")
+matlines(testFaith$waiting[ord], pred1[ord, ], type = "l", col = c(1,2,2), lty = c(1,1,1), lwd = 3)
+
+
+
+
+# Same process with caret
+
+modFit <- train(eruptions ~ waiting, data = trainFaith, method = "lm")
+# outcome: eruptions
+# predictor: wainting
+# data: data set, on which to build it on
+# method: linear modelling
+
+summary(modFit$finalModel)
+
+# Call:
+#   lm(formula = .outcome ~ ., data = dat)
+# 
+# Residuals:
+#   Min       1Q   Median       3Q      Max 
+# -1.26990 -0.34789  0.03979  0.36589  1.05020 
+# 
+# Coefficients:
+#                   |
+#                   V       <- here is about the same result like for our hand made one
+#                Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) -1.792739   0.227869  -7.867 1.04e-12 ***
+#   waiting      0.073901   0.003148  23.474  < 2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.495 on 135 degrees of freedom
+# Multiple R-squared:  0.8032,	Adjusted R-squared:  0.8018 
+# F-statistic:   551 on 1 and 135 DF,  p-value: < 2.2e-16
+
+
+# also interesting 
+modFit
+
+# Linear Regression 
+# 
+# 137 samples
+# 1 predictor
+# 
+# No pre-processing
+# Resampling: Bootstrapped (25 reps) 
+# Summary of sample sizes: 137, 137, 137, 137, 137, 137, ... 
+# Resampling results:
+#   
+#   RMSE       Rsquared   MAE      
+# 0.5043234  0.7976798  0.4092342
+# 
+# Tuning parameter 'intercept' was held constant at a value of TRUE
+
 
 
